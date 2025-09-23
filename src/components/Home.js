7dc97f968ca807115f11341a8ee615df23d3963f
Jin Yu <yjin887@gmail.com>
2025-09-23 12:05:47 +0900

    style(layout): 반응형으로 변경

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 // Home.js
 import React, { useContext } from 'react';
-import { Card, Typography, Button, message } from 'antd';
+import { Card, Typography, Button, message, Layout } from 'antd';
 import { AppstoreOutlined } from '@ant-design/icons';
 
 import { useAuth } from "../context/AuthContext";
 
+const { Content } = Layout;
 const { Title, Text } = Typography;
 
 function Home({ onOpenMap }) {
@@ -16,38 +17,37 @@ function Home({ onOpenMap }) {
   };
 
   return (
-    <div style={{ padding: 24, background: '#rgb(240, 242, 245)', minHeight: '100vh' }}>
-      <Card
-        hoverable
-        style={{ width: '90vw',        // 뷰포트 너비의 90%
-                maxWidth: '1000px',  
-                margin: '0 auto' 
-              }}
-        cover={
-          <div style={{ padding: '40px 0', textAlign: 'center' }}>
-            <AppstoreOutlined style={{ fontSize: 64, color: '#6c5ce7' }} />
-          </div>
-        }
-      >
-        <Title level={3} style={{ textAlign: 'center', marginBottom: 16 }}>
-          {user ? `${user.name} 님, 반갑습니다` : "게스트 님, 반갑습니다"}
-        </Title>
+    <Layout style={{ backgroundColor: '#f5f5f5' }}>
+      <Content style={{ padding: '0 24px 24px 24px' }}>
+        <div style={{ padding: 24, background: '#rgb(240, 242, 245)' }}>
+          <Card
+            cover={
+              <div style={{ padding: '40px 0', textAlign: 'center' }}>
+                <AppstoreOutlined style={{ fontSize: 64, color: '#6c5ce7' }} />
+              </div>
+            }
+          >
+            <Title level={3} style={{ textAlign: 'center', marginBottom: 16 }}>
+              {user ? `${user.name} 님, 반갑습니다` : "게스트 님, 반갑습니다"}
+            </Title>
 
-        <Text type="secondary" style={{ display: 'block', textAlign: 'center', marginBottom: 24 }}>
-          🌍 지도를 보려면 아래 버튼을 클릭하세요
-        </Text>
+            <Text type="secondary" style={{ display: 'block', textAlign: 'center', marginBottom: 24 }}>
+              🌍 지도를 보려면 아래 버튼을 클릭하세요
+            </Text>
 
-        <Button
-          type="primary"
-          size="large"
-          block
-          onClick={handleClick}
-          icon={<AppstoreOutlined />}
-        >
-          지도 보기
-        </Button>
-      </Card>
-    </div>
+            <Button
+              type="primary"
+              size="large"
+              block
+              onClick={handleClick}
+              icon={<AppstoreOutlined />}
+            >
+              지도 보기
+            </Button>
+          </Card>
+        </div>
+      </Content>
+    </Layout>
   );
 }
 
